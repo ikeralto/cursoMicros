23130e4e751a569d6f8c8815f9d1d07b58e2d88f
GFI-ES\iqueralto <ignacio.queralto@inetum.world>
2021-06-23 12:18:05 +0200

    proyectos

---
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
@@ -0,0 +1,16 @@
+package com.formacionbdi.springboot.app.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringbootServicioUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioUsuariosApplication.class, args);
+	}
+
+}
